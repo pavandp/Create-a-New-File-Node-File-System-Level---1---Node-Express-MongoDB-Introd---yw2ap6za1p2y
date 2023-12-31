0e59d45240ad058d3f25276648f4aba4898177af
pavankdp <pavankdp29@gmail.com>
2023-12-31 19:21:23 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try{
+    await fs.writeFile(fileName, fileContent, 'utf-8');
+  }
+  catch(err){
+    console.log(err.message);
+    throw err;
+  }
 };
 
 module.exports = { writeFile };
